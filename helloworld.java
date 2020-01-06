bfb7da485ba709c1d6f878b8727af96aee49f1bd
amonwan <amolvan27@gmail.com>
2020-01-06 11:58:50 +0700

    Q

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
+      Sysyem.out.println("Q");
+
    }
 }
 
